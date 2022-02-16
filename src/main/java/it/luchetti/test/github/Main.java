c8e18289b0b1bb33f043456c423f47585fe43b95
Matteo <elit04tv@gmail.com>
2022-02-16 19:45:56 +0100

    conflitto più grosso

--- src/main/java/it/luchetti/test/github/Main.java
+++ src/main/java/it/luchetti/test/github/Main.java
@@ -7,11 +7,11 @@ package it.luchetti.test.github;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ciao 2 ");
-        System.out.println("ciao 4");
-        System.out.println("albero2");
-        System.out.println("pero");
-        System.out.println("3");
+        System.out.println("ciao8 2 ");
+        System.out.println("ciao8 4");
+        System.out.println("albe8ro2");
+        System.out.println("pero8");
+        System.out.println("348");
     }
     
 }
