32780d10be9db8644a659dfe4e10fbcda1c842cd
Matteo <elit04tv@gmail.com>
2022-02-16 19:40:59 +0100

    nuova classe cane
    attributi++

---
+++ src/main/java/it/luchetti/test/github/Cane.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.luchetti.test.github;
+
+/**
+ *
+ * @author Matteo
+ */
+public class Cane extends Animale{
+    private String nome;
+    private int eta;
+}
