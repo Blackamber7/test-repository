ab5265b62f5de95f99b555147263f42b72706b6f
Matteo <elit04tv@gmail.com>
2022-02-19 11:17:39 +0100

    ho risistemato i nomi delle variabili

--- src/main/java/it/luchetti/test/github/Cane.java
+++ src/main/java/it/luchetti/test/github/Cane.java
@@ -9,6 +9,6 @@ package it.luchetti.test.github;
  * @author Matteo
  */
 public class Cane extends Animale{
-    private String nome3;
-    private int eta4;
+    private String nome;
+    private int eta;
 }
--- src/main/java/it/luchetti/test/github/Main.java
+++ src/main/java/it/luchetti/test/github/Main.java
@@ -7,10 +7,10 @@ package it.luchetti.test.github;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ciao 2 ");
-        System.out.println("ciao 4");
-        System.out.println("albe8ro2");
-        System.out.println("pero8");
+        System.out.println("ciao ");
+        System.out.println("ciao ");
+        System.out.println("albero");
+        System.out.println("pero");
         System.out.println("348");
     }
     
