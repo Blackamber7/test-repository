8b535394429550af5cd94938631d0e42272b24ef
Matteo <elit04tv@gmail.com>
2022-02-19 12:21:49 +0100

    getter e setter
    getter e setter b

--- src/main/java/it/luchetti/test/github/Animale.java
+++ src/main/java/it/luchetti/test/github/Animale.java
@@ -13,6 +13,49 @@ public class Animale {
     private int y;
     private int z;
     private int u;
-    
+
+    public Animale() {
+    }
+
+    public Animale(int x, int y, int z, int u) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.u = u;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getZ() {
+        return z;
+    }
+
+    public void setZ(int z) {
+        this.z = z;
+    }
+
+    public int getU() {
+        return u;
+    }
+
+    public void setU(int u) {
+        this.u = u;
+    }
+
+   
     
 }
--- src/main/java/it/luchetti/test/github/Cane.java
+++ src/main/java/it/luchetti/test/github/Cane.java
@@ -12,4 +12,40 @@ public class Cane extends Animale{
     private String nome;
     private int eta;
     private String razza;
+
+    public Cane() {
+    }
+
+    public Cane(String nome, int eta, String razza) {
+        this.nome = nome;
+        this.eta = eta;
+        this.razza = razza;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public String getRazza() {
+        return razza;
+    }
+
+    public void setRazza(String razza) {
+        this.razza = razza;
+    }
+    
+    
 }
+ 
